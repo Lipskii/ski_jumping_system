dc0f4d88e30396f80ac30492468fc14c19d031aa
Lipskii <46229315+Lipskii@users.noreply.github.com>
2021-06-10 09:51:18 +0200

    ChampionshipEditionsTable.js rebuild completed

--- src/frontend/src/containers/Championships/ChampionshipEditionsTable.js
+++ src/frontend/src/containers/Championships/ChampionshipEditionsTable.js
@@ -15,26 +15,35 @@ const ChampionshipEditionsTable = (props) => {
             fixed: 'left'
         },
         {
-            dataIndex: 'name',
+            dataIndex: 'editionInfo',
             textWrap: 'word-break',
-            render: name =>
+            render: editionInfo =>
                 <div>
-                    {name}
+                    <div>
+                        {editionInfo.name}
+                    </div>
+                    {editionInfo.largeHillCompetitionId !== '' ?
+                        <div>
+                            <a href={'/showResults/' + editionInfo.largeHillCompetitionId}
+                               style={{color: "black", textDecoration: "underline"}} >
+                                <small>
+                                    Large hill results
+                                </small>
+                            </a>
+                        </div>
+                        : null}
+                    {editionInfo.normalHillCompetitionId !== '' ?
+                        <div>
+                        <a href={'/showResults/' + editionInfo.normalHillCompetitionId}
+                           style={{color: "black", textDecoration: "underline"}} >
+                            <small>
+                                Normal hill results
+                            </small>
+                        </a>
+                    </div> : null}
                 </div>,
             fixed: 'left'
         },
-        // {
-        //     dataIndex: 'competitions',
-        //     textWrap: 'word-break',
-        //     render: competitions => competitions.map(competition => (
-        //         <LinkContainer to={'/showResults/' + competition.id}
-        //                        style={{cursor: "pointer", marginTop: "10px", marginBottom: "10px"}}>
-        //             <div>
-        //                 {competition.hillVersion.hill.sizeOfHill.designation}
-        //             </div>
-        //         </LinkContainer>
-        //     )),
-        // },
         {
             title:"Normal Hill",
             children: [
@@ -43,12 +52,6 @@ const ChampionshipEditionsTable = (props) => {
                     dataIndex: 'winnerNormalHill',
                     textWrap: 'word-break',
                     render: winnerNormalHill => winnerNormalHill.map(winnerNormalHill => (
-                        // <LinkContainer to={'/skiJumper/' + winnerNormalHill.skiJumper.id}
-                        //                style={{cursor: "pointer", marginTop: "10px", marginBottom: "10px"}}>
-                        //     <div><img alt={winnerNormalHill.skiJumper.person.country.code}
-                        //               src={props.photos["flag_" + winnerNormalHill.skiJumper.person.country.code]}/> {winnerNormalHill.skiJumper.person.firstName} {winnerNormalHill.skiJumper.person.lastName}
-                        //     </div>
-                        // </LinkContainer>
                         <a href={'/skiJumper/' + winnerNormalHill.skiJumper.id}
                            style={{color: "black"}} >
                             <img alt={winnerNormalHill.skiJumper.person.country.code}
@@ -61,18 +64,11 @@ const ChampionshipEditionsTable = (props) => {
                     dataIndex: 'silverMedalistNormalHill',
                     textWrap: 'word-break',
                     render: silverMedalistNormalHill => silverMedalistNormalHill.map(silverMedalistNormalHill => (
-                        // <LinkContainer to={'/skiJumper/' + silverMedalistNormalHill.skiJumper.id}
-                        //                style={{cursor: "pointer", marginTop: "10px", marginBottom: "10px"}}>
                             <a href={'/skiJumper/' + silverMedalistNormalHill.skiJumper.id}
                                style={{color: "black"}}>
                                 <img alt={silverMedalistNormalHill.skiJumper.person.country.code}
                                           src={props.photos["flag_" + silverMedalistNormalHill.skiJumper.person.country.code]}/> {silverMedalistNormalHill.skiJumper.person.firstName} {silverMedalistNormalHill.skiJumper.person.lastName}
                             </a>
-                            // <div>
-                            //     <img alt={silverMedalistNormalHill.skiJumper.person.country.code}
-                            //           src={props.photos["flag_" + silverMedalistNormalHill.skiJumper.person.country.code]}/> {silverMedalistNormalHill.skiJumper.person.firstName} {silverMedalistNormalHill.skiJumper.person.lastName}
-                            // </div>
-                    //    </LinkContainer>
                     )),
                 },
                 {
@@ -133,46 +129,7 @@ const ChampionshipEditionsTable = (props) => {
                     )),
                 },
             ]
-        },
-        // {
-        //     title: "Winner",
-        //     dataIndex: 'winner',
-        //     textWrap: 'word-break',
-        //     render: winner => winner.map(winner => (
-        //         <LinkContainer to={'/skiJumper/' + winner.skiJumper.id}
-        //                        style={{cursor: "pointer", marginTop: "10px", marginBottom: "10px"}}>
-        //             <div><img alt={winner.skiJumper.person.country.code}
-        //                       src={props.photos["flag_" + winner.skiJumper.person.country.code]}/> {winner.skiJumper.person.firstName} {winner.skiJumper.person.lastName}
-        //             </div>
-        //         </LinkContainer>
-        //     )),
-        // },
-        // {
-        //     title: "2. place",
-        //     dataIndex: 'silverMedalist',
-        //     textWrap: 'word-break',
-        //     render: silverMedalist => silverMedalist.map(silverMedalist => (
-        //         <LinkContainer to={'/skiJumper/' + silverMedalist.skiJumper.id}
-        //                        style={{cursor: "pointer", marginTop: "10px", marginBottom: "10px"}}>
-        //             <div><img alt={silverMedalist.skiJumper.person.country.code}
-        //                       src={props.photos["flag_" + silverMedalist.skiJumper.person.country.code]}/> {silverMedalist.skiJumper.person.firstName} {silverMedalist.skiJumper.person.lastName}
-        //             </div>
-        //         </LinkContainer>
-        //     )),
-        // },
-        // {
-        //     title: "3. place",
-        //     dataIndex: 'bronzeMedalist',
-        //     textWrap: 'word-break',
-        //     render: bronzeMedalist => bronzeMedalist.map(bronzeMedalist => (
-        //         <LinkContainer to={'/skiJumper/' + bronzeMedalist.skiJumper.id}
-        //                        style={{cursor: "pointer", marginTop: "10px", marginBottom: "10px"}}>
-        //             <div><img alt={bronzeMedalist.skiJumper.person.country.code}
-        //                       src={props.photos["flag_" + bronzeMedalist.skiJumper.person.country.code]}/> {bronzeMedalist.skiJumper.person.firstName} {bronzeMedalist.skiJumper.person.lastName}
-        //             </div>
-        //         </LinkContainer>
-        //     )),
-        // },
+        }
     ];
 
     const data = [];
@@ -184,8 +141,15 @@ const ChampionshipEditionsTable = (props) => {
         let winnerLargeHill = []
         let silverMedalistLargeHill = []
         let bronzeMedalistLargeHill = []
+        let editionInfo = {
+            name: edition.competitions[0].hillVersion.hill.venue.city.name + " " + edition.competitions[0].date1.slice(0, 4),
+            largeHillCompetitionId: '',
+            normalHillCompetitionId: ''
+        }
+
         for (const competition of edition.competitions) {
            if(competition.hillVersion.hill.sizeOfHill.id === 4){
+               editionInfo.largeHillCompetitionId = competition.id
                for (const result of competition.results) {
                    if (result.totalRank === 1) {
                        winnerLargeHill.push(result)
@@ -198,6 +162,7 @@ const ChampionshipEditionsTable = (props) => {
                    }
                }
            } else {
+               editionInfo.normalHillCompetitionId = competition.id
                for (const result of competition.results) {
                    if (result.totalRank === 1) {
                        winnerNormalHill.push(result)
@@ -210,8 +175,8 @@ const ChampionshipEditionsTable = (props) => {
                    }
                }
            }
-
         }
+
         data.push({
             key: edition.id,
             winnerNormalHill: winnerNormalHill,
@@ -223,6 +188,8 @@ const ChampionshipEditionsTable = (props) => {
             name: edition.competitions[0].hillVersion.hill.venue.city.name + " " + edition.competitions[0].date1.slice(0, 4),
             countryCode: edition.city.region.country.code,
             competitions: edition.competitions,
+            editionInfo: editionInfo,
+            competitionsInfo: editionInfo,
             ...edition
         })
     }
